39c92c7261822043432258e9f4cb9c1f42211d1d
User <egaraev88@mail.ru>
2022-12-05 11:29:46 +0300

    2. Каркас шахматной доски [#792 #366926]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -24,7 +24,7 @@ public final class Logic {
         for (Figure figure : figures) {
             for (Cell step : steps) {
                 if (figure != null && figure.position.equals(step)) {
-                    throw new OccupiedCellException();
+                    throw new OccupiedCellException("Cell is occupied");
                 }
             }
         }
